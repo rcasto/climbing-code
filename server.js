16e53b49451d57493f623bf387e9c47dcbd4e572
Richard Casto <rcasto92@gmail.com>
2016-11-11 01:19:41 -0800

    trying to setup https with let's encrypt

--- server.js
+++ server.js
@@ -1,11 +1,48 @@
 var path = require('path');
+var http = require('http');
+var https = require('https');
+var fs = require('fs');
 var express = require('express');
+var lex = require('letsencrypt-express');
+// var leChallenge = require('le-challenge-fs');
+// var leStore = require('le-store-certbot');
 
 var config = require('./config.json');
 
 var app = express();
-var port = process.env.PORT || 3000;
+var httpPort = 80;
+var httpsPort = 443;
 
+// Let's Encrypt
+var lexConfig = lex.create({
+    server: 'staging',
+    email: config.email,
+    agreeTos: true,
+    approveDomains: [config.domain]
+});
+
+var httpServer = http.createServer(function (req, res, next) {
+    if (/\/\.well-known\/acme-challenge\/[\w-]+/.test(req.url)) {
+        console.log('!!!!!!!   ACME CHALLENGE !!!!!');
+    } else {
+        res.writeHead(302, {
+            'Location': `https://${req.headers.host}:${httpsPort}${req.url}`
+        });
+        res.end();
+    }
+});
+var httpsServer = https.createServer(lexConfig.httpsOptions, lexConfig.middleware(app));
+
+// Middleware
+app.use(function (req, res, next) {
+    if (req.path === '/manifest.json') {
+        console.log('app manifest header set!');
+        res.set('Content-Type', 'application/manifest+json');
+    } else {
+        console.log('Passthrough:', req.path);
+    }
+    next();
+});
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function (req, res) {
@@ -16,6 +53,10 @@ app.get('/api/config', function (req, res) {
     res.json(config);
 });
 
-app.listen(port, function () {
-    console.log('Server started on port:', port);
+httpServer.listen(httpPort, function () {
+    console.log('Http server listening on port', httpPort);
+});
+
+httpsServer.listen(httpsPort, function () {
+    console.log('Https server listening on port', httpsPort);
 });
