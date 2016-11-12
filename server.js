1871af35bd533e7861f605c8a8d3f0ab3e3c42a7
Richard Casto <rcasto92@gmail.com>
2016-11-12 11:24:57 -0800

    revert web.config to default, serve acme challenges correctly via node

--- server.js
+++ server.js
@@ -30,6 +30,7 @@ app.use(function (req, res, next) {
     }
     next();
 });
+app.use(express.static(path.join(__dirname, '.well-known')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function (req, res) {
